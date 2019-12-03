2815600e1916484de67ed9a805d2f17591493f15
Claudio Gonzalez <claudiojgonzalez@gmail.com>
2019-12-03 00:57:58 -0300

    Adding cities in db

--- populate_db.js
+++ populate_db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
-const Book = require('./models/book')
-const Author = require('./models/author')
-const Category = require('./models/category')
 
 const connexion = 'mongodb://localhost:27017,localhost:27018,localhost:27019/example'
 
 mongoose.connect(`${connexion}`, { replicaSet: 'rs' })
+const Book = require('./models/book')
+const Author = require('./models/author')
+const City = require('./models/city')
+const Category = require('./models/category')
 
 mongoose.connection.once('open', () => {
   console.log('conneted to database')
@@ -15,227 +16,142 @@ function handleError (err) {
   console.log('HUBO UN ERROR:', JSON.stringify(err))
 }
 
+const carlos_paz = new City({
+  name: "carlos paz",
+  population: 50000
+})
+
+const plottier = new City({
+  name: "plottier",
+  population: 40000
+})
+
+const rosario = new City({
+  name: "rosario",
+  population: 1000000
+})
+
+const bariloche = new City({
+  name: "bariloche",
+  population: 100000
+})
+
+carlos_paz.save(function (err) {
+  if (err) return handleError(err);
+})
+
+plottier.save(function (err) {
+  if (err) return handleError(err);
+})
+
+rosario.save(function (err) {
+  if (err) return handleError(err);
+})
+
+bariloche.save(function (err) {
+  if (err) return handleError(err);
+})
+
+
 const junior = new Author({
-  name: 'junior',
-  age: 25
+  name: "junior",
+  age: 25,
+  cityID: plottier._id
 })
 
 junior.save(function (err) {
-  if (err) return handleError(err)
-})
+  if (err) return handleError(err);
+});
+
 
 const claudio = new Author({
-  name: 'claudio',
-  age: 30
+  name: "claudio",
+  age: 30,
+  cityID: carlos_paz._id
 })
 
 claudio.save(function (err) {
-  if (err) return handleError(err)
-})
+  if (err) return handleError(err);
+});
 
 const dario = new Author({
-  name: 'dario',
-  age: 42
+  name: "dario",
+  age: 42,
+  cityID: rosario._id
 })
 
 dario.save(function (err) {
-  if (err) return handleError(err)
-})
+  if (err) return handleError(err);
+});
+
 
 const paola = new Author({
-  name: 'paola',
-  age: 28
+  name: "paola",
+  age: 28,
+  cityID: bariloche._id
 })
 
 paola.save(function (err) {
-  if (err) return handleError(err)
-})
+  if (err) return handleError(err);
+});
 
-console.log(junior._id)
+console.log(junior._id);
+
+
 
 const cat1 = new Category({
-  name: 'economia'
-})
+  name: "economia"
+});
 
-cat1.save()
+cat1.save();
 
 const cat2 = new Category({
-  name: 'ciencia'
-})
+  name: "ciencia"
+});
 
 cat2.save()
+
+
 
 const book1 = new Book({
   _id: new mongoose.Types.ObjectId(),
   pages: 100,
-  name: 'book junior 1',
+  name: "book junior 1",
   authorID: junior._id,
   ISBN: {
-    country: 'argentina',
-    number: 111
+      country: "argentina",
+      number: 111
   },
-  categories: [
-    {
-      name: 'ciencia',
-      category_ID: cat2._id
-    }
+  categories:[
+      {
+          category_ID: cat2._id
+      },
+      {
+          category_ID: cat1._id
+      },
   ]
-})
+});
 
 book1.save(function (err) {
-  if (err) return handleError(err)
-})
+  if (err) return handleError(err);
+});
 
 const book2 = new Book({
   _id: new mongoose.Types.ObjectId(),
   pages: 200,
-  name: 'book claudio 1',
+  name: "book claudio 1",
   authorID: claudio._id,
   ISBN: {
-    country: 'argentina',
-    number: 222
+      country: "argentina",
+      number: 222
   },
-  categories: [
-    {
-      name: 'economia',
-      category_ID: cat1._id
-    }
+  categories:[
+      {
+          category_ID: cat1._id
+      }
   ]
-})
+});
 
 book2.save(function (err) {
-  if (err) return handleError(err)
-})
-
-// /*
-//  Ejemplo: path: "factura.local"
-//  */
-// let factura2 = Factura.aggregate([
-//     {
-//         "$lookup": {
-//             from: 'local',
-//             localField: 'id_local',
-//             foreignField: '_id',
-//             as: 'local',
-//         }
-//     },
-//     { $unwind: { path: "$local", preserveNullAndEmptyArrays: true } },
-//     {
-//         "$lookup": {
-//             from: 'ciudad',
-//             localField: 'local.id_ciudad',
-//             foreignField: '_id',
-//             as: 'local_ciudad'
-//         }
-//     },
-//     { $unwind: { path: "$ciudad", preserveNullAndEmptyArrays: true } },
-// ]).exec(function (err, results) {
-//     //console.log(JSON.stringify(results));
-// });
-
-// /*
-//  Ejemplo: path: "factura.local.nombre_local"
-//  */
-// let factura3 = Factura.aggregate([
-//     {
-//         "$lookup": {
-//             from: 'local',
-//             localField: 'id_local',
-//             foreignField: '_id',
-//             as: 'local',
-//         }
-//     },
-//     { $unwind: { path: "$local", preserveNullAndEmptyArrays: true } },
-//     {
-//         "$lookup": {
-//             from: 'ciudad',
-//             localField: 'local.id_ciudad',
-//             foreignField: '_id',
-//             as: 'local_ciudad'
-//         }
-//     },
-//     { $unwind: { path: "$ciudad", preserveNullAndEmptyArrays: true } },
-//     {
-//         $match: { "local.nombre_local": "carniceria claudio" },
-//     }
-// ])
-//     .exec(function (err, results) {
-//         console.log(JSON.stringify(results));
-//         /*
-//         [
-//            {
-//               "_id":"5dc958cf58cbd4015c324a19",
-//               "tipo":"A",
-//               "concepto":"pago",
-//               "id_local":"5dc958cf58cbd4015c324a15",
-//               "items":[
-//                  {
-//                     "_id":"5dc958cf58cbd4015c324a1a",
-//                     "nombre_item":"Matambre"
-//                  }
-//               ],
-//               "__v":0,
-//               "local":{
-//                  "_id":"5dc958cf58cbd4015c324a15",
-//                  "nombre_local":"carniceria claudio",
-//                  "id_ciudad":"5dc958cf58cbd4015c324a13",
-//                  "__v":0
-//               },
-//               "local_ciudad":[
-//                  {
-//                     "_id":"5dc958cf58cbd4015c324a13",
-//                     "nombre_ciudad":"carlos paz",
-//                     "id_provincia":"5dc958cf58cbd4015c324a11",
-//                     "__v":0
-//                  }
-//               ]
-//            }
-//         ]
-//         */
-//     });
-
-// /*
-// */
-// let factura4 = Factura.aggregate([
-//     {
-//         $lookup: {
-//             from: "local",
-//             localField: "id_local",    // field in the orders collection
-//             foreignField: "_id",  // field in the items collection
-//             as: "full_factura"
-//         }
-//     },
-//     {
-//         $replaceRoot: { newRoot: { $mergeObjects: [{ $arrayElemAt: ["$full_factura", 0] }, "$$ROOT"] } }
-//     },
-//     { $project: { full_factura: 0 } },
-//     {
-//         $lookup: {
-//             from: "ciudad",
-//             localField: "id_ciudad",    // field in the orders collection
-//             foreignField: "_id",  // field in the items collection
-//             as: "full_factura2"
-//         }
-//     },
-//     {
-//         $replaceRoot: { newRoot: { $mergeObjects: [{ $arrayElemAt: ["$full_factura2", 0] }, "$$ROOT"] } }
-//     },
-//     { $project: { full_factura2: 0 } },
-//     {
-//         $lookup: {
-//             from: "provincia",
-//             localField: "id_provincia",    // field in the orders collection
-//             foreignField: "_id",  // field in the items collection
-//             as: "full_factura3"
-//         }
-//     },
-//     {
-//         $replaceRoot: { newRoot: { $mergeObjects: [{ $arrayElemAt: ["$full_factura3", 0] }, "$$ROOT"] } }
-//     },
-//     { $project: { full_factura3: 0 } },
-
-// ]).exec(function (err, results) {
-//     //console.log(JSON.stringify(results));
-// });
-
-// console.log("---------- finished ----------");
+  if (err) return handleError(err);
+});
